b82c56a09deb60ccb0632c26f06727a0f362857a
Kanish Kumar <kanishkumar11@outlook.in>
2023-04-12 14:43:45 +0530

    test

--- app/head.js
+++ app/head.js
@@ -2,7 +2,7 @@ const Head = () => (
   <>
     <title>Metaversus</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="KanishKumar11/metaverse-landing-page/public/favicon.ico" />
+    <link rel="icon"src="./favicon.ico" />
   </>
 );
 
